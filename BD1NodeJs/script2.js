59427b29335cb2dcbe4a2f3f86e5bad89d6b124f
arpit gupta <rajarpitgupta@gmail.com>
2026-04-20 16:01:23 +0530

    New loop

--- BD1NodeJs/script2.js
+++ BD1NodeJs/script2.js
@@ -1,11 +1,22 @@
-let n = 5;
+// let n = 5;
 
-for(let i=0; i<n; i++) {
-    console.log("hello, ", i);
-}
-console.log("Bye");
+// for(let i=0; i<n; i++) {
+//     console.log("hello, ", i);
+// }
+// console.log("Bye");
 
-for(let i=0; i<3; i++) {
-    console.log("Arpit"+i);
+// for(let i=0; i<3; i++) {
+//     console.log("Arpit"+i);
+// }
+// console.log("WebDevelopment");
+
+
+// console.log(process.argv);
+
+let args = process.argv;
+
+
+for(let i=2; i<args.length; i++) {
+    console.log("Hello to", args[i]);
+
 }
-console.log("WebDevelopment");
