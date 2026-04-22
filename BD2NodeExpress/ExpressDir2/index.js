9fdfdec21fa0855dbf2ec28b5fef5b57b01ce9ac
arpit gupta <rajarpitgupta@gmail.com>
2026-04-22 16:46:16 +0530

    post res add

--- BD2NodeExpress/ExpressDir2/index.js
+++ BD2NodeExpress/ExpressDir2/index.js
@@ -9,14 +9,31 @@ myApp.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
 
-myApp.use((req, res) => {
-    // console.log(res);
-    console.log("request recieved");
-    // res.send("this is a basic response");
-    // res.send ({
-    //     name : "apple",
-    //     color: "red",
-    // });
-    let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Orange</li></ul>"; 
-    res.send(code);
+// myApp.use((req, res) => {
+//     // console.log(res);
+//     console.log("request recieved");
+//     // res.send("this is a basic response");
+//     // res.send ({
+//     //     name : "apple",
+//     //     color: "red",
+//     // });
+//     let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Orange</li></ul>"; 
+//     res.send(code);
+// });
+
+
+myApp.get("/", (req, res) => {
+    res.send("you contacted root path");
+});
+
+myApp.get("/apple", (req, res) => {
+    res.send("you contacted apple path");
+});
+
+myApp.get("/help", (req, res) => {
+    res.send("you contacted help path");
+});
+
+myApp.post("/", (req, res) => {
+    res.send("you sent a post request to root");
 });
