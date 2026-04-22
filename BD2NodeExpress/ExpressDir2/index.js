0c9ee4e89fac595ba3e9941f79440aeb482cf36d
arpit gupta <rajarpitgupta@gmail.com>
2026-04-22 17:01:01 +0530

    app post update

--- BD2NodeExpress/ExpressDir2/index.js
+++ BD2NodeExpress/ExpressDir2/index.js
@@ -23,17 +23,26 @@ myApp.listen(port, () => {
 
 
 myApp.get("/", (req, res) => {
-    res.send("you contacted root path");
+    res.send("Hello i am root");
 });
 
-myApp.get("/apple", (req, res) => {
-    res.send("you contacted apple path");
-});
+// myApp.get("/apple", (req, res) => {
+//     res.send("you contacted apple path");
+// });
 
-myApp.get("/help", (req, res) => {
-    res.send("you contacted help path");
-});
+// myApp.get("/help", (req, res) => {
+//     res.send("you contacted help path");
+// });
 
-myApp.post("/", (req, res) => {
-    res.send("you sent a post request to root");
-});
+// myApp.post("/", (req, res) => {
+//     res.send("you sent a post request to root");
+// });
+
+myApp.get("/:username/:id", (req, res) => {
+    // console.log(req.params);
+    // res.send("Hello I am root");
+
+    let {username, id} = req.params;
+    let htmlStr = `<h1>Welcome to the page of @${username}</h1>`
+    res.send(htmlStr);
+}); 
