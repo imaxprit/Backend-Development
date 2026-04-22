b8ee03f02b6b74de609a75173a1b3cf49721ada6
arpit gupta <rajarpitgupta@gmail.com>
2026-04-22 16:33:24 +0530

    response added

--- BD2NodeExpress/ExpressDir2/index.js
+++ BD2NodeExpress/ExpressDir2/index.js
@@ -8,3 +8,15 @@ let port = 8080;
 myApp.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
+
+myApp.use((req, res) => {
+    // console.log(res);
+    console.log("request recieved");
+    // res.send("this is a basic response");
+    // res.send ({
+    //     name : "apple",
+    //     color: "red",
+    // });
+    let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Orange</li></ul>"; 
+    res.send(code);
+});
