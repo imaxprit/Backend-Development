88b21ff8fde4df672261ecab37e42ad4d051a34a
arpit gupta <rajarpitgupta@gmail.com>
2026-04-27 16:17:45 +0530

    new file created

---
+++ Miscellaneous/BackendFold/index.js
@@ -0,0 +1,15 @@
+const express = request("express");
+const app = express();
+const port = 8080;
+
+app.get("/register", (req, res) => {
+    res.send("Standard GET response");
+});
+
+app.post("/register", (req, res) => {
+    res.send("Standard POST response");
+});
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+});
