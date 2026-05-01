3b22fb380fd884f7f0d28f5ac2700821ef912597
arpit gupta <rajarpitgupta@gmail.com>
2026-05-01 14:42:14 +0530

    added new

---
+++ Miscellaneous/FrontendFold/app.js
@@ -0,0 +1,26 @@
+const stu1 = {
+    name : "Rahul",
+    age : 25,
+    marks : 78,
+    getMarks : function() {
+        return this.marks;
+    }
+};
+
+const stu2 = {
+    name : "Arpit",
+    age : 21,
+    marks : 87,
+    getMarks : function() {
+        return this.marks;
+    }
+};
+
+const stu3 = {
+    name : "Suraj",
+    age : 23,
+    marks : 65,
+    getMarks : function() {
+        return this.marks;
+    }
+}
