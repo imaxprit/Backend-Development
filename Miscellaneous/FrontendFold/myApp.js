d616059c800aa87e62382a0378a0974af2b7b003
arpit gupta <rajarpitgupta@gmail.com>
2026-05-02 08:55:01 +0530

    oop in js

---
+++ Miscellaneous/FrontendFold/myApp.js
@@ -0,0 +1,17 @@
+const emp1 = {
+    name : "Arpit",
+    age : 21,
+    empId : 121,
+    getInfo : function() {
+        console.log(`This is ${this.name}`);
+    },
+};
+
+const emp2 = {
+    name : "Amit",
+    age : 25,
+    empId : 124,
+    getInfo : function() {
+        console.log(`This is ${this.name}`);
+    }
+}
