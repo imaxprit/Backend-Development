726def0599c84fcac3fb3d0b7e7c34516f86dd99
arpit gupta <rajarpitgupta@gmail.com>
2026-05-25 11:24:40 +0530

    crudOpt

---
+++ MONGO/index.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main()
+    .then((res) => {
+        console.log("Connection Successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+// const Employee = mongoose.model("Employee", userSchema);
+
+// Insert in Mongoose
+
+// const user2 = new User({
+//     name: "Eve",
+//     email: "eve@google.com",
+//     age: 38,
+// });
+
+// user2
+//     .save()
+//     .then((res) => {
+//         console.log(res);
+//     }) 
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// User.insertMany([
+//     {name: "Tony", email: "tony@gmail.com", age: 50},
+//     {name: "Bruce", email: "bruce@gmail.com", age: 47},
+//     {name: "Peter", email: "peter@gmail.com", age: 30},
+// ]).then((res) => {
+//     console.log(res);
+// });
+
+// Find in Mongoose
+
+// User.findOne({age: {$gte: 48}})
+// User.findOne({_id: '6a13df05950efdd653e00f77'})
+User.findById('6a13df05950efdd653e00f77')
+    .then((res) => {
+        // console.log(res[0].name);
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
