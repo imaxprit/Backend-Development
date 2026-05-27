95024f1da5496484e842a23935a386fc17265fa0
arpit gupta <rajarpitgupta@gmail.com>
2026-05-27 15:51:11 +0530

    Add some opt

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
     .then(() => {
@@ -23,6 +24,32 @@ app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
     console.log(chats);
     res.render("index.ejs", {chats});
+});
+
+// New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+// Create Route
+app.post("/chats", (req, res) => {
+    let {from, to, msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    });
+    // console.log(newChat);
+    newChat
+    .save()
+    .then((res) => {
+        console.log("chat was saved")
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
