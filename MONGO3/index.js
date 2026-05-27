6ce7340346d8526f62045c811b94def5d5dc32b7
arpit gupta <rajarpitgupta@gmail.com>
2026-05-27 11:17:47 +0530

    chat Model

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+main()
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let chat1 = new Chat({
+    from : "Arpit",
+    to : "Rahul",
+    msg : "how are you?",
+    created_at : new Date()
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+})
+
+app.get("/", (req, res) => {
+    res.send("Root is working");
+});
 
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
